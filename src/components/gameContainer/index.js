5f6c566378602e915b06a84fa7d731023b37dec7
Luke Haines <lukehaines2@gmail.com>
2019-05-09 02:42:13 +0100

    add round system and readme update

--- src/components/gameContainer/index.js
+++ src/components/gameContainer/index.js
@@ -4,19 +4,18 @@ import { SimonButton } from "../simonButton";
 import { GameControls } from "../gameControls";
 import "./gameContainer.scss";
 
-const red = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3"); 
-const blue = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3"); 
-const green = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3"); 
+const red = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");
+const blue = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3");
+const green = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3");
 const yellow = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3");
 
-const buttonSounds = [red, blue, green, yellow]
+const buttonSounds = [red, blue, green, yellow];
 const buttons = ["red", "blue", "green", "yellow"];
 
-
-const generateSeries = (min, max) => {
-  const series = []
-  for (let i = 0; i < 10; i++) {
-    series.push(Math.floor(Math.random() * max))
+const generateSeries = () => {
+  const series = [];
+  for (let i = 0; i < 20; i++) {
+    series.push(Math.floor(Math.random() * 4));
   }
   return series
 }
@@ -27,11 +26,13 @@ export default class SimonContainer extends React.Component {
     super(props);
     this.playSound = this.playSound.bind(this);
     this.playSequence = this.playSequence.bind(this);
+    this.startGame = this.startGame.bind(this);
     this.state = {
       soundPlaying: false,
-      sequence: generateSeries(0, 4),
+      sequence: generateSeries(),
       score: 0,
-      answered: 0
+      answered: 0,
+      level: 1
     }
   }
 
@@ -44,32 +45,54 @@ export default class SimonContainer extends React.Component {
       if (shouldEval) {
         this.evalAnswer(index);
       }
-    }, 900 ));
+    }, 900));
   }
 
-  playSequence() {
-    const { sequence } = this.state;
+  getSeqByLevel(level) {
+    return this.state.sequence.slice(0, level);
+  }
+
+  startGame() {
+    // Slice first index from sequence
+    const seq = this.getSeqByLevel(1);
+    this.playSequence(seq);      
+  };
+
+  playSequence(seq) {
     let i = 0;
 
     const interval = setInterval(() => {
-      this.playSound(sequence[i]);
+      this.playSound(seq[i]);
       i++;
-      if (i >= sequence.length) {
+      if (i >= seq.length) {
         clearInterval(interval);
       }
-    }, 1000)
+    }, 1000);
   }
 
   evalAnswer(ans) {
-    const { sequence, answered, score } = this.state;
-    const answer = sequence[answered];
+    const { sequence, answered, level, score } = this.state;
+    const seq = this.getSeqByLevel(level);
+    // console.log(answered, seq.length);
+
+    if (answered === seq.length) {
+      // console.log("hit if");
+      const nextSeq = this.getSeqByLevel(level+1);
+      this.nextLevel(nextSeq);
+    }
     
+    const answer = sequence[answered];
     if (ans === answer) {
       // correct answer
-      this.setState({ 
-        answered: answered+1,
-        score: score+1
-      });
+      if (answered === 0 && seq.length < 2) {
+        const nextSeq = this.getSeqByLevel(level+1);
+        this.nextLevel(nextSeq);
+      } else {
+        this.setState({
+          answered: answered+1,
+          score: score+1,
+        });
+      }
     } else {
       // wrong answer
       this.resetScores();
@@ -77,22 +100,31 @@ export default class SimonContainer extends React.Component {
     }
   }
 
+  nextLevel(seq) {
+    this.setState({
+      answered: 0,
+      score: 0,
+      level: this.state.level+1
+    })
+    this.playSequence(seq);
+  }
+
   resetScores() {
     this.setState({
       answered: 0,
       score: 0
-    })
+    });
   }
 
   render() {
-    const { sequence, soundPlaying, score } = this.state;
+    const { sequence, soundPlaying, score, level } = this.state;
     return (
       <div className="gameContainer">
         <h1>Simon Game</h1>
         {buttons.map((name, i) => (
-          <SimonButton key={name} name={name} index={i} handlePlaySound={this.playSound} {...{soundPlaying}} />
+          <SimonButton key={name} {...{soundPlaying}} name={name} index={i} handlePlaySound={this.playSound} />
         ))}
-        {<GameControls {...{sequence, score, soundPlaying}} playSequence={this.playSequence} />}
+        <GameControls {...{sequence, score, level, soundPlaying}} startGame={this.startGame} />
       </div>
     )
   }
--- src/components/gameControls/index.js
+++ src/components/gameControls/index.js
@@ -4,7 +4,7 @@ import "./gameControls.scss";
 
 export const GameControls = props => {
   const onClick = () => {
-    props.playSequence();
+    props.startGame();
   };
 
   return (
@@ -12,6 +12,7 @@ export const GameControls = props => {
       <button className='btn btn-danger start' onClick={onClick}>Start</button>
       <p>Sound Playing: {props.soundPlaying}</p>
       <p>Score: {props.score}</p>
+      <p>Level: {props.level}</p>
       ----
       <p>For cheaters! -> {props.sequence}</p>
     </div>
